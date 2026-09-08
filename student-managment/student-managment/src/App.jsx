801f78b555a245f31f3108e123f3153a0aa385b5
S kiarn <soudrikiran@gmail.com>
2026-09-08 16:26:41 +0530

    js completed and project started

---
+++ student-managment/student-managment/src/App.jsx
@@ -0,0 +1,9 @@
+import Welcom from "./components/Welcom";
+function App() {
+  return (
+    <div> 
+      <Welcom />
+    </div>
+  )
+}
+export default App;
---
+++ student-managment/student-managment/src/components/Welcom.jsx
@@ -0,0 +1,7 @@
+function Welcom(){
+    return (
+            <h1>Welcome to student managment portal</h1>
+        
+    );
+}
+export default Welcom;
